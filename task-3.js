5369ad0dbe7b6c672461dd0ebbc07b77bd8777a3
Rafi1072 <rafi110-96@o2.pl>
2024-07-14 13:49:08 +0200

    first-try-of-task-3

---
+++ task-3.js
@@ -0,0 +1,13 @@
+function filterArray(numbers, value) {
+  for (const neewArray of numbers) {
+    if (numbers.length > value) {
+      return neewArray.push(numbers.slice(value));
+    }
+  }
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
